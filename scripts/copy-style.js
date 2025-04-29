52e12aecb4c4802b69f5f15910a340d2be7fbaa2
Canale0107 <canale0107@icloud.com>
2025-04-30 04:05:54 +0900

    copy-style.jsのパスを試行錯誤

--- scripts/copy-style.js
+++ scripts/copy-style.js
@@ -2,8 +2,12 @@ import { copyFile } from "fs/promises";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// Vercelでもローカルでも、正しいプロジェクトルートを得る
-const projectRoot = process.cwd();
+// このファイルの絶対パス
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// __dirname = /vercel/path0/scripts
+const projectRoot = path.resolve(__dirname, ".."); // /vercel/path0
 
 const src = path.join(projectRoot, "../src/styles/style.css");
 const dest = path.join(projectRoot, "../public/styles/style.css");
