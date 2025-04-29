21a7ebc74dc34a63b427e47a804fbc623a048164
Canale0107 <canale0107@icloud.com>
2025-04-30 04:02:32 +0900

    copy-style.jsのパスをカレントディレクトリから取得

--- scripts/copy-style.js
+++ scripts/copy-style.js
@@ -2,11 +2,8 @@ import { copyFile } from "fs/promises";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// プロジェクトルートを起点にする
-const projectRoot = path.resolve(__dirname, "..");
+// Vercelでもローカルでも、正しいプロジェクトルートを得る
+const projectRoot = process.cwd();
 
 const src = path.join(projectRoot, "../src/styles/style.css");
 const dest = path.join(projectRoot, "../public/styles/style.css");
