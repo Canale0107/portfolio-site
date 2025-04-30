7592544030eeefa15159b512e462cd31f71c9b35
Canale0107 <canale0107@icloud.com>
2025-05-01 00:48:59 +0900

    ディレクトリ名を小文字に戻した

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 // src/react/App.jsx
 import React, { useEffect, useState } from "react";
-import Navbar from "@/components/Navbar/Navbar";
-import Profile from "@/components/Profile/Profile";
-import Overview from "@/components/Overview/Overview";
-import Career from "@/components/Career/Career";
-import Skills from "@/components/Skills/Skills";
-import Research from "@/components/Research/Research";
-import Interests from "@/components/Interests/Interests";
+import Navbar from "@/components/navbar/Navbar";
+import Profile from "@/components/profile/Profile";
+import Overview from "@/components/overview/Overview";
+import Career from "@/components/career/Career";
+import Skills from "@/components/skills/Skills";
+import Research from "@/components/research/Research";
+import Interests from "@/components/interests/Interests";
 import Footer from "@/components/Footer";
 
 export default function App() {
@@ -27,7 +27,7 @@ export default function App() {
       <Navbar toggleTheme={toggleTheme} theme={theme} />
       <main className="profile-page" id="top">
         <h1 className="visually-hidden">小寺奏怜｜プロフィール</h1>
-        <Profile theme={theme}/>
+        <Profile theme={theme} />
         <Overview />
         <Career />
         <Skills />
