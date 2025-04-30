4b39bcfa027a5ce40782f4cf0210f13e32f37e60
Canale0107 <canale0107@icloud.com>
2025-04-30 16:21:29 +0900

    価値観をJSON化

--- src/components/overview/Values.jsx
+++ src/components/overview/Values.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import values from "@/data/values.json";
+
 export default function Values() {
   return (
     <section id="values">
@@ -6,23 +9,7 @@ export default function Values() {
         <span className="sec-ja">大切にする価値観</span>
       </h2>
       <div className="overview__values">
-        {[
-          {
-            label: "美学",
-            text: `哲学に通じる強いこだわりを持ち、思考や行動における美しさと一貫性を重んじる。<br />
-            論理と感性が交差する場所に、真の美を見出したいと願っている。`,
-          },
-          {
-            label: "平静",
-            text: `判断においては常に冷静かつ客観的であることを心がける。<br />
-            ただし、感情を排除せず、それもまた人間的な理解の一部として大切にしたい。`,
-          },
-          {
-            label: "公正",
-            text: `表面的な対立や単純な二項対立に囚われず、<br />
-            背後にある構造や文脈、多様な視点に目を向けて思考することを大切にしている。`,
-          },
-        ].map((item, i) => (
+        {values.map((item, i) => (
           <div className="overview__value" key={i}>
             <div className="overview__value-icon">{item.label}</div>
             <p dangerouslySetInnerHTML={{ __html: item.text }} />
